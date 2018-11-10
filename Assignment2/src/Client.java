b1fcc0658120a51d4265d0c94a763b58d6d0515e
Sean Kearney <s.kearney.94@gmail.com>
2018-11-10 22:47:15 +0000

    Added Login screen which connects to an SQL server to authenticate.

--- Assignment2/src/Client.java
+++ Assignment2/src/Client.java
@@ -2,11 +2,14 @@ import java.io.*;
 import java.net.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+import com.mysql.cj.xdevapi.Statement;
 
 public class Client extends JFrame implements ActionListener{
 	
@@ -23,10 +26,10 @@ public class Client extends JFrame implements ActionListener{
 	private JButton btnSend, btnExit;
 	
 	// Text field for receiving message
-	private JTextField jtf = new JTextField();
+	private JTextField jtf;
  
 	// Text area to display contents
-	private JTextArea jta = new JTextArea();
+	private JTextArea jta;
 
 	// IO streams
 	private DataOutputStream toServer;
@@ -48,36 +51,18 @@ public class Client extends JFrame implements ActionListener{
 		System.out.println("Connected to SQL Server");
 		return conn;
 	}
-
+	
 	public Client() throws SQLException {
-	    // Panel p to hold the label and text field
-	    JPanel p = new JPanel();
-	    p.setLayout(new BorderLayout());
-	    p.add(new JLabel("Enter message"), BorderLayout.WEST);
-	    p.add(jtf, BorderLayout.CENTER);
-	    jtf.setHorizontalAlignment(JTextField.RIGHT);
-
-	    setLayout(new BorderLayout());
-	    add(p, BorderLayout.NORTH);
-	    btnSend = new JButton("Send");
-	    btnSend.addActionListener(this);
-		add(btnSend, BorderLayout.WEST);
-	    add(new JScrollPane(jta), BorderLayout.CENTER);
-	    btnExit = new JButton("Exit");
-	    btnExit.addActionListener(this);
-		add(btnExit, BorderLayout.EAST);
-	    jtf.addActionListener(this); // Register listener
-
-	    setTitle("Client");
+		setLayout(new CardLayout());
+		jtf = new JTextField("Message");
+		add(makeGuiLogin());
+		add(makeGuiMain());
+		
+		setTitle("Client");
 	    setSize(500, 300);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setVisible(true); // It is necessary to show the frame here!
 
-	    //connect to SQL Server
-	    Connection conn = getConnection();
-	    
-	    
-	    
 	    try {
 	      // Create a socket to connect to the server
 	      Socket socket = new Socket("localhost", 8000);
@@ -92,6 +77,95 @@ public class Client extends JFrame implements ActionListener{
 	    }
 	  }
 
+	private JPanel makeGuiMain() {
+		// Panel p to hold the label and text field
+	    JPanel main = new JPanel();
+	    main.setLayout(new BorderLayout());
+	    
+	    JPanel p = new JPanel();
+	    p.setLayout(new BorderLayout());
+	    jtf.addActionListener(this); // Register listener
+	    p.add(jtf, BorderLayout.CENTER);
+	    p.add(new JLabel("Enter message"), BorderLayout.WEST);
+	    main.add(p, BorderLayout.NORTH);
+	    
+	    btnSend = new JButton("Send");
+	    btnSend.addActionListener(this);
+	    main.add(btnSend, BorderLayout.WEST);
+	    jta = new JTextArea();
+	    main.add(new JScrollPane(jta), BorderLayout.CENTER);
+	    btnExit = new JButton("Exit");
+	    btnExit.addActionListener(this);
+	    main.add(btnExit, BorderLayout.EAST);
+	    
+	    return main;
+	}
+	
+	private JPanel makeGuiLogin(){
+		JPanel main = new JPanel();
+	    main.setLayout(new BorderLayout());
+	    main.add(new JLabel("Enter Sudent ID:"), BorderLayout.WEST);
+	    //text field for student ID
+	    JTextField stuNum = new JTextField("Sudent Number");
+	    stuNum.setPreferredSize( new Dimension( 20, 20) );
+		main.add(stuNum);
+	    JButton t = new JButton("Login");
+	    
+	    t.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				CardLayout cl = (CardLayout)(getContentPane().getLayout());
+				
+				try {
+					Connection conn = getConnection();
+					if(testInt(stuNum.getText())== true) {
+						int ID = Integer.parseInt(stuNum.getText());
+						String user = authenticate(ID, conn);
+						if(user .equals("")) {
+							JOptionPane.showMessageDialog(null, "User does not exist");
+						}else {
+							JOptionPane.showMessageDialog(null, "Welcome " + user);
+							cl.next(getContentPane());
+						}	
+					}else {
+						JOptionPane.showMessageDialog(null, "Invalid entry");
+					}
+				} catch (SQLException e) {
+					System.err.println(e + "NO SQL Connection");
+				}
+			}
+	    });
+	    main.add(t, BorderLayout.EAST);
+	    return main;
+	}
+	
+	Boolean testInt(String test) {
+		try {
+	        Integer.parseInt(test);
+	        return true;
+	    }
+	    catch( Exception e ) {
+	        return false;
+	    }
+	}
+	
+	@SuppressWarnings("finally")
+	String authenticate(int number, Connection conn) {
+		String command = "SELECT * FROM mystudents \n WHERE STUD_ID ="+number+";";
+		java.sql.Statement s = null;
+		ResultSet rs = null;
+		String result = "";
+	    try {
+	    	s = conn.createStatement ();
+			s.executeQuery(command);
+			rs = s.getResultSet();
+			rs.next();
+			result = rs.getString(3) + " " + rs.getString(4);
+	    }finally {
+	    	return result;
+	    }
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {		
 		if (e.getSource() == btnSend) {
