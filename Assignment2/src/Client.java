c5228ac9c5415903f7c5c06c3e87cdbddfa8191d
Sean Kearney <s.kearney.94@gmail.com>
2018-11-11 18:31:25 +0000

    Moved authentication from client to server.
    Messages now show IP address of sender
    Login screen now handles invalid inputs properly.

--- Assignment2/src/Client.java
+++ Assignment2/src/Client.java
@@ -1,26 +1,11 @@
 import java.io.*;
 import java.net.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-import com.mysql.cj.xdevapi.Statement;
-
 public class Client extends JFrame implements ActionListener{
-	
-	//for connecting to SQL Server
-	private final String userName = "root";
-	private final String password = "";
-	private final String serverName = "localhost";
-	private final int portNumber = 3306;
-	private final String dbName = "assign2";
-	private final String tableName = "mystudents";
-	
 	
 	//make buttons
 	private JButton btnSend, btnExit;
@@ -38,19 +23,6 @@ public class Client extends JFrame implements ActionListener{
 	public static void main(String[] args) throws SQLException {
 		new Client();
 	}
-  
-	public Connection getConnection() throws SQLException {
-		Connection conn = null;
-		Properties connectionProps = new Properties();
-		connectionProps.put("user", this.userName);
-		connectionProps.put("password", this.password);
-		//the "?useSSL=False needs to be here to avoid an error/warning with MYSQL on my computer"
-		conn = DriverManager.getConnection("jdbc:mysql://"
-				+ this.serverName + ":" + this.portNumber + "/" + this.dbName+"?useSSL=false",
-				connectionProps);
-		System.out.println("Connected to SQL Server");
-		return conn;
-	}
 	
 	public Client() throws SQLException {
 		setLayout(new CardLayout());
@@ -62,23 +34,23 @@ public class Client extends JFrame implements ActionListener{
 	    setSize(500, 300);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setVisible(true); // It is necessary to show the frame here!
-
-	    try {
-	      // Create a socket to connect to the server
-	      Socket socket = new Socket("localhost", 8000);
-	      // Create an input stream to receive data from the server
-	      fromServer = new DataInputStream(socket.getInputStream());
-
-	      // Create an output stream to send data to the server
-	      toServer = new DataOutputStream(socket.getOutputStream());
-	    }
-	    catch (IOException ex) {
-	      jta.append(ex.toString() + '\n');
-	    }
 	  }
+	
+	void serverConnect() {
+		try {
+		      // Create a socket to connect to the server
+		      Socket socket = new Socket("localhost", 8000);
+		      // Create data input and output stream to sennd and receive data from the server
+		      fromServer = new DataInputStream(socket.getInputStream());
+		      toServer = new DataOutputStream(socket.getOutputStream());  
+		    }
+		    catch (IOException ex) {
+		      jta.append(ex.toString() + '\n');
+		    }
+	}
 
 	private JPanel makeGuiMain() {
-		// Panel p to hold the label and text field
+		// Main panel for when user logs in.
 	    JPanel main = new JPanel();
 	    main.setLayout(new BorderLayout());
 	    
@@ -102,69 +74,61 @@ public class Client extends JFrame implements ActionListener{
 	}
 	
 	private JPanel makeGuiLogin(){
+		//Login panel to allow user to authenticate
 		JPanel main = new JPanel();
 	    main.setLayout(new BorderLayout());
 	    main.add(new JLabel("Enter Sudent ID:"), BorderLayout.WEST);
 	    //text field for student ID
 	    JTextField stuNum = new JTextField("Sudent Number");
-	    stuNum.setPreferredSize( new Dimension( 20, 20) );
 		main.add(stuNum);
-	    JButton t = new JButton("Login");
+	    JButton loginBtn = new JButton("Login");
 	    
-	    t.addActionListener(new ActionListener() {
+	    
+	    loginBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				CardLayout cl = (CardLayout)(getContentPane().getLayout());
-				
-				try {
-					Connection conn = getConnection();
-					if(testInt(stuNum.getText())== true) {
-						int ID = Integer.parseInt(stuNum.getText());
-						String user = authenticate(ID, conn);
+				//test if the input is valid(int only)
+				if(testInt(stuNum.getText())== true) {
+					
+					try {
+						//connect to server
+					    serverConnect();
+					    //send user ID to server
+						String ID = stuNum.getText().trim();
+						toServer.writeUTF(ID);
+						//Read result from server and load the main view if a valid username is returned
+						String user = fromServer.readUTF();
 						if(user .equals("")) {
 							JOptionPane.showMessageDialog(null, "User does not exist");
 						}else {
 							JOptionPane.showMessageDialog(null, "Welcome " + user);
 							cl.next(getContentPane());
-						}	
-					}else {
-						JOptionPane.showMessageDialog(null, "Invalid entry");
+						}
+					} catch (IOException e) {
+						System.out.println(e);
 					}
-				} catch (SQLException e) {
-					System.err.println(e + "NO SQL Connection");
+				}else {
+					JOptionPane.showMessageDialog(null, "Invalid entry, Numerical characters only");
 				}
+
 			}
 	    });
-	    main.add(t, BorderLayout.EAST);
+	    
+	    main.add(loginBtn, BorderLayout.EAST);
 	    return main;
 	}
 	
-	Boolean testInt(String test) {
+	//test if string can be cast to int
+	 Boolean testInt(String test) {
 		try {
-	        Integer.parseInt(test);
-	        return true;
-	    }
-	    catch( Exception e ) {
-	        return false;
-	    }
-	}
-	
-	@SuppressWarnings("finally")
-	String authenticate(int number, Connection conn) {
-		String command = "SELECT * FROM mystudents \n WHERE STUD_ID ="+number+";";
-		java.sql.Statement s = null;
-		ResultSet rs = null;
-		String result = "";
-	    try {
-	    	s = conn.createStatement ();
-			s.executeQuery(command);
-			rs = s.getResultSet();
-			rs.next();
-			result = rs.getString(3) + " " + rs.getString(4);
-	    }finally {
-	    	return result;
-	    }
-	}
+		       Integer.parseInt(test);
+		       return true;
+		   }
+		   catch( Exception e ) {
+		       return false;
+		   }
+	 }
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {		
@@ -187,3 +151,5 @@ public class Client extends JFrame implements ActionListener{
 		}
 	}
 }
+
+
--- Assignment2/src/Server.java
+++ Assignment2/src/Server.java
@@ -1,10 +1,23 @@
 import java.io.*;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 
 public class Server extends JFrame {
+	
+	//for connecting to SQL Server
+		private final String userName = "root";
+		private final String password = "";
+		private final String serverName = "localhost";
+		private final int portNumber = 3306;
+		private final String dbName = "assign2";
+	
+	
 	// Text area for displaying contents
 	  private JTextArea jta = new JTextArea();
 
@@ -20,31 +33,94 @@ public class Server extends JFrame {
 	    setSize(500, 300);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setVisible(true); // It is necessary to show the frame here!
-
+	    
 	    try {
-	    	
+	    	// Get connection to SQL Server
+	    	Connection conn = getConnection();
 	    	// Create a server socket
 		    ServerSocket serverSocket = new ServerSocket(8000);
 		    jta.append("Server started at " + new Date() + '\n');
 
 		    while (true) {	 
+		    	//Create socket for client
 		    	Socket s1=serverSocket.accept();
+
 		    	myClient c = new myClient(s1);
-		    	c.start();
+		    	int stuNum = 0;
+		    	//Get student number entry from client and convert it to int
+		    	String sFromClient = c.fromClient.readUTF();
+		    	if (testInt(sFromClient) == true) {
+		    		stuNum = Integer.parseInt(sFromClient);
+		    	}
+		    	//get corresponding username from the SQL server and send it back to the client
+		    	String userName = authenticate(stuNum, conn);
+		    	c.toClient.writeUTF(userName);
+		    	//If the username is valid start a thread else close the socket
+		    	if(!userName .equals("")){
+		    		c.start();
+		    	} else {
+		    		s1.close();
+		    	}
+		    	
 		    }
 	    }
 	    catch(IOException ex) {
 	    	System.err.println(ex);
-	    }
+	    } catch (SQLException e) {
+	    	System.err.println(e);
+		}
 	 } 
-	      
+	  //test if string can be cast to int
+	  Boolean testInt(String test) {
+			try {
+		        Integer.parseInt(test);
+		        return true;
+		    }
+		    catch( Exception e ) {
+		        return false;
+		    }
+		}
+	  
+	  //get connection to SQL server
+	  public Connection getConnection() throws SQLException {
+			Connection conn = null;
+			Properties connectionProps = new Properties();
+			connectionProps.put("user", this.userName);
+			connectionProps.put("password", this.password);
+			//"?useSSL=False to avoid an error/warning with MYSQL"
+			conn = DriverManager.getConnection("jdbc:mysql://"
+					+ this.serverName + ":" + this.portNumber + "/" + this.dbName+"?useSSL=false",
+					connectionProps);
+			System.out.println("Connected to SQL Server");
+			return conn;
+		}
+	  
+	  //check if user exists and return back name of user
+	  @SuppressWarnings("finally")
+		String authenticate(int number, Connection conn) {
+			String command = "SELECT * FROM mystudents \n WHERE STUD_ID ="+number+";";
+			java.sql.Statement s = null;
+			ResultSet rs = null;
+			String result = "";
+		    try {
+		    	s = conn.createStatement ();
+				s.executeQuery(command);
+				rs = s.getResultSet();
+				rs.next();
+				result = rs.getString(3) + " " + rs.getString(4);
+		    }finally {
+		    	return result;
+		    }
+		}
 	  
 	  private class myClient extends Thread {
+		private InetAddress address;
 		private DataInputStream fromClient;
 		private DataOutputStream toClient;
 		// The Constructor for the client
 		public myClient(Socket socket) throws IOException {
-			//Initialise input/output streams	
+			//Initialise input/output streams and IP Address	
+			address = socket.getInetAddress();
 			fromClient = new DataInputStream(socket.getInputStream());
 			toClient = new DataOutputStream(socket.getOutputStream());
 		}
@@ -52,11 +128,11 @@ public class Server extends JFrame {
 		
 		public void run() {
 			try {
-				// Send+Receive
+				// Send+Receive messages
 				while (true) {
 					String message = fromClient.readUTF();
 			        toClient.writeChars(message);
-			        jta.append("Message: " + message + '\n');
+			        jta.append(address + ": "+ message + '\n');
 				}
 			} catch (Exception e) {
 				System.err.println(e + " Error");
